a1f8a6e2234a34e6014d7d8f6c2b199ba5172674
Kevin Groat <apps@kevingroat.io>
2017-09-10 19:19:34 -0400

    Renamed color generator

--- src/generators/misc/color.ts
+++ src/generators/misc/color.ts
@@ -12,7 +12,7 @@ const HUE_MAX = 360
 const SAT_MAX = 100
 const VAL_MAX = 100
 
-export function generate ({ h, s, v }: ColorDefinition) {
+export function generateColor ({ h, s, v }: ColorDefinition) {
   const ch = ((Math.random() * (h[1] - h[0]) + h[0]) + HUE_MAX) % HUE_MAX
   const cs = ((Math.random() * (s[1] - s[0]) + s[0]) + SAT_MAX) % SAT_MAX
   const cv = ((Math.random() * (v[1] - v[0]) + v[0]) + VAL_MAX) % VAL_MAX
--- src/generators/rockGenerator.ts
+++ src/generators/rockGenerator.ts
@@ -1,6 +1,6 @@
 
 import { RocksSchema } from '../schemas/rocks'
-import { generate } from './misc/color'
+import { generateColor } from './misc/color'
 import { Rock } from '../interfaces/rock'
 import { choose } from '../helpers'
 
@@ -9,6 +9,6 @@ const rockDetails = require('../details/rocks.json') as RocksSchema
 export function generateRock(): Rock {
   const colorModel = choose(rockDetails.color)
   return {
-    color: generate(colorModel)
+    color: generateColor(colorModel)
   }
 }
